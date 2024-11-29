a2f79a73bffaf6b8a1c3485582ef52eea1ce5655
Pauline Metay <metaypauline@gmail.com>
2024-11-29 11:27:15 +0100

    enhanced-version

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -16,6 +16,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
